f5def2e470fa89cd63aff3e954b38a6e3d548fc6
mooting <justin_w_davis@yahoo.com>
2018-12-29 12:49:34 -0800

    solved

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,14 @@
+require 'pry'
 def reformat_languages(languages)
-  # your code here
+  new_hash = {}
+  languages.map do |style, language_list|
+    language_list.map do |language, type|
+      if new_hash[language]
+        new_hash[language][:style] << style
+      else
+        new_hash[language] = {:type => type[:type], :style => [style]}
+      end
+    end
+  end
+  new_hash
 end
